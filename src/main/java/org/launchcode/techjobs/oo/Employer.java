0532fb86b6bc8dc77e313947a4aaadaeda273f77
i-swa <ievaswanson@u.boisestate.edu>
2024-04-27 15:14:54 -0500

    Completed task 1 and added comments for reference

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -2,17 +2,28 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
+// Sally's demo of how to assign a unique ID:
 public class Employer {
 
+    // Two fields: ID and value
     private int id;
+
+    // Because nextId is static, its changing value is NOT stored within any Employer object
     private static int nextId = 1;
     private String value;
 
+    // Two constructors: Employer() and Employer(String value):
+
+    // Employer () assigns the value of nextId to the id field
     public Employer() {
         id = nextId;
+        // Increments so every new Employer object will get a different ID number
         nextId++;
     }
 
+    // Employer (String value) assigns aValue to the value field
+    // Employer (String, value) initializes id for the object by calling the first constructor with the this(); statement
+    // Including this(); in any Employer constructor makes initializing id a default behavior
     public Employer(String value) {
         this();
         this.value = value;
