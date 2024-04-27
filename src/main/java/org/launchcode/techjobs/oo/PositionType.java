5346a030628ec788cd61cea464c3ed548a502bf2
i-swa <ievaswanson@u.boisestate.edu>
2024-04-27 16:16:29 -0500

    coded toString()

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,6 +1,7 @@
 package org.launchcode.techjobs.oo;
 
 public class PositionType {
+    // The constructors, getters, and setters are completed in the starter code.
 
     private int id;
     private static int nextId = 1;
@@ -17,9 +18,17 @@ public class PositionType {
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
+    // Code a toString method that just returns the value of a PositionType object.
+    @Override
+    public String toString() {
+        return value;
+    }
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+    //Use the Generate option again to add the equals and hashCode methods.
+    // Refer to the Classes and Objects Part 2 IntelliJ Generator Shortcut section if you need a quick review.
+    //Assume that two PositionType objects are equal when their id fields match.
 
     // Getters and Setters:
 
